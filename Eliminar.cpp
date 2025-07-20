849986ac84e7ecaacf2195575c79ed3c89ac421b
Justin Paladines <justin.paladines@epn.edu.ec>
2025-07-19 22:13:55 -0500

    agregada funcion guardar_tarea

--- Eliminar.cpp
+++ Eliminar.cpp
@@ -11,6 +11,8 @@ struct Tarea {
     bool estado; // false = Pendiente, true = Completada
 };
 
+vector<Tarea>tareas = {}; 
+
 void eliminarTarea(vector<Tarea>& tareas, int numero) {
     for (auto it = tareas.begin(); it != tareas.end(); ++it) {
         if (it->numero_tarea == numero) {
@@ -20,4 +22,21 @@ void eliminarTarea(vector<Tarea>& tareas, int numero) {
         }
     }
     cout << "No se encontró una tarea con ese número.\n";
+}
+
+//creare una funcion para guardar el archivo
+//y se modifiquen los cambios al eliminar una tarea
+void guardarTarea(const vector<Tarea>& tareas, const string archivoTareas){
+	//uso ofstream para escribir en el archivo
+	ofstream archivo(archivoTareas);
+	if(!archivo.is_open()){
+		cerr<<"Error! no se pudo abrir el archivo."<<endl;
+		return;
+		//explicado por el ingeniero, en void se puede usar return, para salir
+	}
+	for(const auto& tarea_cargar: tareas){
+		string estado = tarea_cargar.estado ? "pendiente" : "completada";
+		archivo<<tarea_cargar.numero_tarea<<"|"<<
+		tarea_cargar.descripcion<<"|"<<estado<<endl;
+	}
 }
